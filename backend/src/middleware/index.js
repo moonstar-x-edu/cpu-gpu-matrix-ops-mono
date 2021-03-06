48eaae7503b653444038e3d70e2cdfc343713dfe
moonstar-x <christian.lopez99@outlook.com>
2021-03-06 01:15:22 -0500

    Created response generation middleware and utils.

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -1,9 +1,21 @@
+const { HTTP_CODES } = require('../constants');
+const { generateResponse } = require('../utils');
+
 const allowCORS = (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 };
 
+const onlySupportedMethods = (methods) => {
+  return (_, res) => {
+    res.header('Access-Control-Allow-Methods', methods.join(' '));
+    return res.status(HTTP_CODES.METHOD_NOT_ALLOWED)
+      .send(generateResponse(HTTP_CODES.METHOD_NOT_ALLOWED));
+  };
+};
+
 module.exports = {
-  allowCORS
+  allowCORS,
+  onlySupportedMethods
 };
