b55fe1abdce7a1b56bbf40fe6969d5e494f4b69d
moonstar-x <christian.lopez99@outlook.com>
2021-03-06 18:47:23 -0500

    Integrated example chart in results for testing.

--- webapp/src/components/results/Results.jsx
+++ webapp/src/components/results/Results.jsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import ResultBarChart from '../common/resultBarChart';
 import AppContext from '../../context/AppContext';
 import { NAVBAR_ITEMS } from '../../constants';
 import { updatePageTitle } from '../../utils/page';
@@ -11,10 +13,32 @@ const Results = () => {
     updatePageTitle('Resultados');
   }, [setActive]);
 
+  const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: '# of Red Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: 'rgb(255, 99, 132)'
+      },
+      {
+        label: '# of Blue Votes',
+        data: [2, 3, 20, 5, 1, 4],
+        backgroundColor: 'rgb(54, 162, 235)'
+      },
+      {
+        label: '# of Green Votes',
+        data: [3, 10, 13, 15, 22, 30],
+        backgroundColor: 'rgb(75, 192, 192)'
+      }
+    ]
+  };
+
   return (
-    <div>
+    <Container className="results-content">
       RESULTS
-    </div>
+      <ResultBarChart data={data} redraw />
+    </Container>
   );
 };
 
