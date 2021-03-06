a3317500ca8bd1ac36104dd148c13dfe73ef8950
moonstar-x <christian.lopez99@outlook.com>
2021-03-06 01:15:34 -0500

    Using results router in main express app.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const logger = require('@greencoast/logger');
-const { allowCORS } = require('./middleware');
+const { allowCORS, onlySupportedMethods } = require('./middleware');
+const resultsRouter = require('./api/results');
 const { HTTP_CODES } = require('./constants');
+const { generateResponse } = require('./utils');
 
 const HTTP_PORT = process.env.HTTP_PORT || 8080;
 
 const app = express();
 app.use(allowCORS);
 
+app.use('/results', resultsRouter);
+
 app.get('/', (req, res) => {
-  res.status(HTTP_CODES.OK).send('IT WORKS!');
+  res.status(HTTP_CODES.OK).send(generateResponse(HTTP_CODES.OK, 'It works!'));
 });
 
+app.all('/', onlySupportedMethods(['GET']));
+
 app.all('*', (req, res) => {
-  res.status(HTTP_CODES.NOT_FOUND).send('Not Found...');
+  res.status(HTTP_CODES.NOT_FOUND).send(generateResponse(HTTP_CODES.NOT_FOUND, new Error('This route is not handled by the server.')));
 });
 
 app.listen(HTTP_PORT, () => {
