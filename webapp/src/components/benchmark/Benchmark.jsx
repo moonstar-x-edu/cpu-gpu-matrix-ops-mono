c787c15ee1b0d47e2dc0c8832707a84d33d069b2
moonstar-x <christian.lopez99@outlook.com>
2021-03-05 23:03:04 -0500

    Changed page title on all pages.

--- webapp/src/components/benchmark/Benchmark.jsx
+++ webapp/src/components/benchmark/Benchmark.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from 'react';
 import AppContext from '../../context/AppContext';
 import { NAVBAR_ITEMS } from '../../constants';
+import { updatePageTitle } from '../../utils/page';
 
 const Benchmark = () => {
   const { setActive } = useContext(AppContext);
 
   useEffect(() => {
     setActive(NAVBAR_ITEMS.benchmark);
+    updatePageTitle('Benchmark');
   }, [setActive]);
 
   return (
--- webapp/src/components/notFound/NotFound.jsx
+++ webapp/src/components/notFound/NotFound.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import AppContext from '../../context/AppContext';
+import { updatePageTitle } from '../../utils/page';
 
 const NotFound = () => {
   const { setActive } = useContext(AppContext);
 
   useEffect(() => {
     setActive(null);
+    updatePageTitle('¡Página no encontrada!');
   }, [setActive]);
 
   return (
