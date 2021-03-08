da75f9fb1805a898f33d14a72740ad18692ecfc0
moonstar-x <christian.lopez99@outlook.com>
2021-03-08 16:11:08 -0500

    Created a LoadingSpinner component.

--- webapp/src/constants/index.js
+++ webapp/src/constants/index.js
@@ -12,3 +12,15 @@ export const NAVBAR_ITEMS = {
 export const LINKS = {
   github: 'https://github.com/moonstar-x-edu/cpu-gpu-matrix-ops-mono'
 };
+
+export const BS_COLOR_VARIANTS = {
+  blue: 'primary',
+  gray: 'secondary',
+  green: 'success',
+  red: 'danger',
+  yellow: 'warning',
+  cyan: 'info',
+  white: 'light',
+  black: 'dark',
+  custom: null
+};
