873671fcc2872771d1a10812c1f965be6618c425
moonstar-x <christian.lopez99@outlook.com>
2021-03-08 16:10:56 -0500

    Added a loading state to ResultsContext.

--- webapp/src/components/results/Results.jsx
+++ webapp/src/components/results/Results.jsx
@@ -15,22 +15,41 @@ const colors = {
 
 const Results = () => {
   const { setActive } = useContext(AppContext);
-  const { allResults, setAllResults, fetchError, setFetchError } = useContext(ResultsContext);
+  const { allResults, setAllResults, fetchError, setFetchError, loading, setLoading } = useContext(ResultsContext);
 
   useEffect(() => {
     setActive(NAVBAR_ITEMS.results);
     updatePageTitle('Resultados');
 
-    if (!allResults) {
+    if (!allResults && !fetchError && !loading) {
+      setLoading(true);
       getAllResults()
         .then((results) => {
           setAllResults(results);
+          setLoading(false);
         })
         .catch((error) => {
           setFetchError(error);
+          setLoading(false);
         });
     }
-  }, [allResults, setActive, setAllResults, setFetchError]);
+  }, [allResults, setActive, setAllResults, fetchError, setFetchError, loading, setLoading]);
+
+  if (loading) {
+    return (
+      <Container className="results-content">
+        LOADING
+      </Container>
+    );
+  }
+
+  if (fetchError) {
+    return (
+      <Container className="results-content">
+        ERROR FETCHING!
+      </Container>
+    );
+  }
 
   return (
     <Container className="results-content">
